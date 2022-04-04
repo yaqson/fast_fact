5a758a8f106a8e6873eb44460cdfa89cd0549629
yaqson <yink4life@gmail.com>
2022-04-04 12:59:40 +0100

    javascript class

---
+++ app.js
@@ -0,0 +1,14 @@
+let myName = "yaqson"
+console.log
+let x = 8;
+let y = 5;
+z= 8-5;
+console.log(8-5)
+f = "father";
+m = "mother";
+let father = 8;
+let mother = 6;
+z = (father + mother)
+
+let myName="adeYinka";
+let MyName="Adeyinka";
